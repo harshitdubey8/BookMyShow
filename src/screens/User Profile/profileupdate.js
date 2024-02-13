33e27ade50f2562f9baaf3bb8ee79ae85b28a1dc
Harshit Dubey <hdlikes8@gmail.com>
2024-02-14 00:35:15 +0530

    fixed bugs

--- src/screens/User Profile/profileupdate.js
+++ src/screens/User Profile/profileupdate.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./profileUpdate.css";
 import CustomButton from "../../components/CustomButton";
 import axios from "axios";
